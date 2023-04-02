cfa674c270d3556691518376c9b94bbbb3b51b8c
Keks <keks@htmlacademy.ru>
2023-04-02 21:05:31 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/render-full-size-photo.js
@@ -0,0 +1,22 @@
+import './thumbnails.js';
+import {onClickThumbnail, createCommentsList, renderComments} from './full-size-photo.js';
+
+const containerPictures = document.querySelector('.pictures');
+const fullSizePhoto = document.querySelector('.big-picture');
+const socialComments = fullSizePhoto.querySelector('.social__comments');
+const commentsLoader = fullSizePhoto.querySelector('.social__comments-loader');
+
+const renderFullSizePhoto = (pictures) => {
+  containerPictures.addEventListener('click', (evt) => {
+    const thumbnail = evt.target.closest('[data-thumbnail-index]');
+    if(!thumbnail){
+      return;
+    }
+    const photoItem = pictures.find((item)=> item.id === +thumbnail.dataset.thumbnailIndex);
+    onClickThumbnail(photoItem);
+    commentsLoader.classList.remove('hidden');
+    createCommentsList(photoItem.comments, socialComments);
+    renderComments();
+  });
+};
+export {renderFullSizePhoto};
